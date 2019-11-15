f0c8ec8a4d82945432f33ec5074499a8cf989035
Arkworce <41407837+Arkworce@users.noreply.github.com>
2019-11-15 10:54:43 +0800

    Update 11单词压缩储存.c

--- 11单词压缩储存.c
+++ 11单词压缩储存.c
@@ -101,7 +101,51 @@ SNODE * findlist( SNODE * head1, SNODE * head2 )
 
 SNODE * ziplist( SNODE * head1, SNODE * head2 )
 {
-	; 
+	int m, n, flag = 0, len = 0;
+	SNODE *p1 = head1, *p2 = head2, *ans = NULL, *temp = NULL, *temp2;
+	
+	m = listlen(head1);
+	n = listlen(head2);
+	
+	while(m > n)
+	{
+		p1 = p1->next;
+		m --;
+	}
+	while(m < n)
+	{
+		p2 = p2->next;
+		n --;
+	}
+	while(m > 0)
+	{
+		if(p1->next->data == p2->next->data)
+		{
+			if(flag == 0)
+			{
+				ans = p1;
+				temp = p2;
+			}
+			flag = 1;
+		}
+		else
+		{
+			flag = 0;
+		}
+		p1 = p1->next;
+		p2 = p2->next;
+		m --;
+	}
+
+	temp2 = temp->next;
+	temp->next = ans->next;
+	while(temp2 != NULL)
+	{
+		temp = temp2->next;
+		free(temp2);
+		temp2 = temp;
+	}
+	return ans->next;
 }
 
 /* PRESET CODE END - NEVER TOUCH CODE ABOVE */
